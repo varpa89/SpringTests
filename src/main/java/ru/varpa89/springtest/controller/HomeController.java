c091d652732df94e27eb6f8b27fad18d51c9cf0e
pasha <varpa89@gmail.com>
2014-03-28 23:53:42 +0400

    hello spring

--- src/main/java/ru/varpa89/springtest/controller/HomeController.java
+++ src/main/java/ru/varpa89/springtest/controller/HomeController.java
@@ -1,7 +1,20 @@
 package ru.varpa89.springtest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by pasha on 28.03.2014.
  */
+@Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String loadIndexPage(Model model){
+        model.addAttribute("title", "Hello Spring");
+        return "index";
+    }
 }
